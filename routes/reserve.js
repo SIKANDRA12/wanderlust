730e261d972e1a8cf03ce23a895a0dd1d80cb0f4
Delta student <sikandra8231@gmail.com>
2025-04-10 21:08:46 +0530

    added 10

--- routes/reserve.js
+++ routes/reserve.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const { isLoggedIn } = require('../middleware.js');  // if you have this
 const { reserveListing } = require('../controllers/reserveController');
 
-router.post('/reserve/:id', reserveListing);
+// Protecting Route (Optional but recommended)
+router.post('/reserve/:id', isLoggedIn, reserveListing);
 
 module.exports = router;
